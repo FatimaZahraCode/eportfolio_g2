c6054f842bee5af0f02c656a21554aff7aa94952
FATIMA ZAHRA <1728829@alu.murciaeduca.es>
2025-12-03 08:25:07 +0100

    proyecto actualizado

--- resources/views/familias-profesionales/create.blade.php
+++ resources/views/familias-profesionales/create.blade.php
@@ -5,11 +5,7 @@
     @section('content')
         <h2>Create Familias Profesionales</h2>
 
-<<<<<<< HEAD
-         <div class="row" style="margin-top:40px">
-=======
           <div class="row" style="margin-top:40px">
->>>>>>> 39fc365be0664deebe708235540996afd3615840
         <div class="offset-md-3 col-md-6">
             <div class="card">
                 <div class="card-header text-center">
@@ -17,43 +13,16 @@
                 </div>
                 <div class="card-body" style="padding:30px">
 
-<<<<<<< HEAD
-                    <form action="{{ action([App\Http\Controllers\ProyectosController::class, 'store']) }}" method="POST">
-
-                        @csrf
-
-=======
                     <form action="{{ action([App\Http\Controllers\FamiliasProfesionalesController::class, 'store']) }}" method="POST">
 
                         @csrf
 
 
->>>>>>> 39fc365be0664deebe708235540996afd3615840
                         <div class="form-group">
                             <label for="nombre">Nombre</label>
                             <input type="text" name="nombre" id="nombre" class="form-control">
                         </div>
 
-<<<<<<< HEAD
-                        <div class="form-group">
-                            <label for="docente_id">Docente</label>
-                            <input type="number" name="docente_id" id="docente_id">
-                        </div>
-
-                        <div class="form-group">
-                            <label for="dominio">Dominio</label><br />
-                            https://github.com/2DAW-CarlosIII/
-                            <input type="text" name="dominio" id="dominio" class="form-control">
-                        </div>
-
-                        <div class="form-group">
-                            <label for="metadatos">Metadatos</label>
-                            <textarea name="metadatos" id="metadatos" class="form-control" rows="3"></textarea>
-                            <br /><small>Cada metadato irá separado del siguiente por una línea <br />
-                                y la clave irá separada por : del valor</small>
-                        </div>
-
-=======
 
                         <div class="form-group">
 
@@ -62,28 +31,18 @@
                         </div>
 
 
->>>>>>> 39fc365be0664deebe708235540996afd3615840
                         <div class="form-group text-center">
                             <button type="submit" class="btn btn-primary" style="padding:8px 100px;margin-top:25px;">
                                 Añadir familia profesional
                             </button>
                         </div>
-<<<<<<< HEAD
-
                     </form>
-
-=======
-                    </form>
->>>>>>> 39fc365be0664deebe708235540996afd3615840
                 </div>
             </div>
         </div>
     </div>
-<<<<<<< HEAD
-=======
 
 
 
 
->>>>>>> 39fc365be0664deebe708235540996afd3615840
     @endsection
